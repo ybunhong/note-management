d8152c513a7b92ca547fcaad69d6773e2686e444
ybunhong <ybunhon369@gmail.com>
2025-11-13 15:18:08 +0700

    init: file structure frontend and backend

---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,7 @@
+import NotePage from "./pages/note-page";
+
+function App() {
+  return (<NotePage />)
+}
+
+export default App
---
+++ apps/frontend/src/pages/note-page.tsx
@@ -0,0 +1,10 @@
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+
+export default function NotePage() {
+    return (
+        <Container>
+            <Button variant="contained" >hello bro</Button>           
+        </Container>
+    )
+}
